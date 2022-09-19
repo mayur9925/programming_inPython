18592f39ebb8b7eed3096d173ed4cd8a9dd4cab4
mayur9925 <113876806+mayur9925@users.noreply.github.com>
2022-09-19 10:16:52 +0530

    Add files via upload

---
+++ mnb+-+.py
@@ -0,0 +1,28 @@
+#email_id = input('enter your email_id')
+#mobile_no = input('enter your mobile.no')
+
+#print(type(email_id))
+#print(type(mobile_no))
+           
+#singer = input('enter your singer')
+#song = input('enter your song')
+
+#print(type(singer))
+#print(type(song))
+
+
+#a =int(input('enter the num1'))
+#b =int(input('enter the num2'))
+#c = a-b+a
+#print (c)       
+
+
+
+a=int(input('enter the num1'))
+b=int(input('enter the num2'))
+c=int(input('enter the num3'))
+d=int(input('enter the num4'))
+e= a-b+c-d
+
+print (e)
+
